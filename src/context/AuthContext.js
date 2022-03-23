ed97c1612da531e80b5c0083c42cab02ffeb64cb
brianpompey <brianthescholar@gmail.com>
2022-03-23 16:23:43 -0400

    Added actions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,12 +9,28 @@ const authReducer = (state, action) => {
 
 const signup = (dispatch) => {
     return ({ email, password }) => {
+        //api request with email and password
+        //if we sign up modify our state and say we are authenticated
+        // if signup fails we need to send an error message
+    };
+};
 
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        //ry to signin
+        //handle success by updating state
+        //handle failure with error message
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        //signout
     };
 };
 
 export const { Provider, Context } = createDataContext (
     authReducer,
-    {},
+    {signin, signout, signup},
     { isSignedIn: false }
 );
